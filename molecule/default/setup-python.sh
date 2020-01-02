e4dd4925525d155b27132f7c316c7d1cab61cb63
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-01-02 08:14:50 -0300

    os-family/darwin: explicit support for MacOSX; improved testinfra tests; Arch Linux debug

--- molecule/default/setup-python.sh
+++ molecule/default/setup-python.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 # Install python3 on Alpine linux (Docker)
-if ! [ -v "$(command -v apk)" ]; then
+if [ "$(command -v apk)" ]; then
   apk add python3
   exit 0
 fi
+
 # Install python 3 on Arch Linux (if already does not have)
-if ! [ -v "$(command -v pacman)" ]; then
-  pacman -S python
+if [ "$(command -v pacman)" ]; then
+  pacman -Sy python
+
+  # To detect where is python
+  # pacman -Sy which
+  #/bin/sh which python
+
   exit 0
 fi
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,14 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root' or f.group == 'wheel'
+
+
+def test_groups(host):
+    assert host.group("group1").exists
+    assert host.group("group1").exists
+
+
+def test_users(host):
+    assert host.user("user1").exists
+    assert host.user("user2").exists
+    assert "group2" in host.user("user2").groups
