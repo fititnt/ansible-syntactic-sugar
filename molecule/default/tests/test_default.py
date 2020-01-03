3334ce5c5eab90809723256b052e66d8d53ceceb
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-01-02 23:51:11 -0300

    Added files/cdn (with symlinks); improved molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,11 +16,24 @@ def test_hosts_file(host):
 
 
 def test_groups(host):
-    assert host.group("group1").exists
-    assert host.group("group1").exists
+    assert host.group("cdns").exists
+    # assert host.group("group1").exists
 
 
 def test_users(host):
-    assert host.user("user1").exists
-    assert host.user("user2").exists
-    assert "group2" in host.user("user2").groups
+    assert host.user("cdn-site-a").exists
+    assert host.user("cdn-site-b").exists
+    assert "cdns" in host.user("cdn-site-a").groups
+    assert "cdns" in host.user("cdn-site-b").groups
+
+
+def test_sample_concent_cdns(host):
+    fa = host.file('/home/cdn-site-a/public_html/index.html')
+    assert fa.exists
+    assert fa.user == 'cdn-site-a'
+    assert fa.group == "cdns"
+
+    fb = host.file('/home/cdn-site-b/public_html/index.html')
+    assert fb.exists
+    assert fb.user == 'cdn-site-b'
+    assert fb.group == "cdns"
